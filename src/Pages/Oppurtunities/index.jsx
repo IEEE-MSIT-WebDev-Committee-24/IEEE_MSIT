e6bec74a09b72ae0277d507966b3b0f494a1a4b4
karan singh bisht <karansbisht7@gmail.com>
2024-03-26 13:39:24 +0530

    Oppurtunities page created

---
+++ src/Pages/Oppurtunities/index.jsx
@@ -0,0 +1,29 @@
+import "./style.css";
+
+const Oppurtunities = () =>{
+    return(
+    <>
+        <div id="container">
+            <div className="details">
+                <h3>Scholarship</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, harum a! Delectus qui, tempore beatae doloribus repudiandae dolor mollitia culpa, reiciendis aut, nostrum dolores atque quia. Quo, nisi! Recusandae, nihil.
+                Quis dolores aspernatur error tempore, dolor illo voluptatem sapiente assumenda doloremque, quod corrupti earum unde? Quisquam reprehenderit obcaecati quam doloribus amet aut dolores nostrum dolorem, reiciendis officiis, tenetur ut harum.
+                Exercitationem totam odio iure facilis! Animi ratione odit ea id. Magni harum modi veniam debitis, possimus labore id ex dolore incidunt ipsa, atque laborum exercitationem nihil doloremque et laboriosam voluptatem?</p>
+            </div>
+            <div className="details">
+                <h3>Funding</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem sapiente voluptatem, culpa blanditiis distinctio cumque iusto adipisci non commodi aliquam iure, harum asperiores numquam. Animi commodi eveniet necessitatibus. Autem, hic.
+                Atque rem voluptate aliquam. Nesciunt libero ipsa tempora enim, exercitationem impedit. Adipisci repudiandae deleniti cumque, ab nam, fuga dignissimos non sapiente nisi quo alias explicabo pariatur consectetur atque facere itaque?</p>
+            </div>
+            <div className="details">
+                <h3>Grants</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non recusandae excepturi, officia voluptas cupiditate doloremque reprehenderit aut fugiat maxime sint sequi aspernatur repudiandae soluta optio repellendus sapiente error. Ad, cumque?
+                Ad repudiandae maiores voluptates impedit aliquam iure aperiam quam ipsa omnis maxime. Distinctio et fugiat perferendis molestiae illo debitis aliquid id voluptatum vel, tempore laborum quibusdam ea, itaque nesciunt. Explicabo?</p>
+            </div>
+        </div>
+    </>
+    )
+}
+
+export default Oppurtunities;
+
