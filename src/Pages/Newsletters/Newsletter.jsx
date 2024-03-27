3ff65b3bb50a59edb33dfe7d72b72ec9b88adf6d
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-03-28 01:44:17 +0530

    Added logo to newsletter

--- src/Pages/Newsletters/Newsletter.jsx
+++ src/Pages/Newsletters/Newsletter.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import HTMLFlipBook from 'react-pageflip'
+import Logo from './assets/ieee-logo.png'
 
 const Newsletter = ({ pages,index }) => {
   return (
     <div className="flipbook-container">
       <HTMLFlipBook width={400} height={500} className='flipbook-container'>
-        <p className='intro'>Newsletter {index+1}</p>
+        <div className='intro'>
+        <img src={Logo} alt="IEEE" className='logo' />
+        <p>Newsletter {index+1}</p>
+        </div>
         {pages.map((page, index) => (
           <img key={index} src={page} alt={`Page ${index + 1}`} className="flipbook-page"  />
         ))}
