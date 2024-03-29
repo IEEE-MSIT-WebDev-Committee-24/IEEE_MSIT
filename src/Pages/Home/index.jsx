5547bdbb0dd89b66b1f9fcedbf9e274077771cad
meetarora <meet.arora2403@gmail.com>
2024-03-29 23:01:26 +0530

    second commit

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,13 @@
 import Events from "../../components/Home/Events";
 import Head from "../../components/Home/Head";
 import Home from "../../components/Home/Home";
-
+import AwardList from "../../components/Home/Awards";
 const HomePage = () => {
   return (
     <>
       <Head />
       <Home />
+      <AwardList />
       <Events />
     </>
   );
